8cc5afd1d5cbc9fc5266c98c213cc8b6d917adbe
cherryS77 <94288172+cherryS77@users.noreply.github.com>
2023-05-02 19:13:58 +0200

    first commit

--- src/AlexioHead.js
+++ src/AlexioHead.js
@@ -6,8 +6,8 @@ const AlexioHead = () => {
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        name="keywords"
+        content="fitness, obozy, trening"
       />
       {/*
      ========================================================================
